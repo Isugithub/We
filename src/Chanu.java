d6e75f26a827cfe2301b113209af5f7a1b6977aa
Isugithub <isulakshani57@gmail.com>
2023-01-13 16:37:58 +0530

    Initial commit

---
+++ src/Chanu.java
@@ -0,0 +1,11 @@
+public class Chanu {
+    Chanu(){
+        String x = "isu"; //this is a example for the constructor
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        Chanu c = new Chanu();
+    }
+}
+//Thanksfor watching
